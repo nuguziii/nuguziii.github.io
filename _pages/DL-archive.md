e17086823472c9dab4e27586828a7077cdc0a204
Chae Eun Lee <nuguziii@naver.com>
2019-08-03 01:04:36 +0900

    _

---
+++ _pages/DL-archive.md
@@ -0,0 +1,7 @@
+---
+title: "Deep Learning"
+layout: categories
+permalink: /DL/
+author_profile: true
+---
+딥러닝 관련 글 모음입니다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,16 @@
-var store = []
+var store = [{
+        "title": "[논문읽기] A Style-Based Generator Architecture for Generative Adversarial Networks",
+        "excerpt":"이 글은 2019 CVPR 논문, A Style-Based Generator Architecture for Generative Adversarial Networks를 참고하여 작성하였습니다. NVIDIA의 StyleGAN 논문 리뷰로 첫 블로그 글을 작성하게 되었네요. 이미 올해 초에 큰 화제가 되었고, CVPR에서도 큰 관심을 받았던 논문이라고 들었습니다. 그만큼 다른 리뷰글들이 이미 많이 있지만, 저도 그 글들을 참고해서 한번 리뷰해보겠습니다:) StyleGAN을 통해...","categories": ["Paper Review"],
+        "tags": ["GAN"],
+        "url": "http://localhost:4000/paper%20review/PR-001/",
+        "teaser":null},{
+        "title": "[논문읽기] Noise2Noise: Learning Image Restoration without Clean Data",
+        "excerpt":"이 글은 2018 ICML 논문, Noise2Noise: Learning Image Restoration without Clean Data와 TechTalksTV 영상을 참고하여 작성하였습니다. 이 논문은 작년에 발표되었던 논문이지만, 읽었을 당시 매우 인상깊어서 꼭 블로그 글로 작성해보고 싶었습니다. Image Denoising 주제에 대해 관심이 많았었는데, 기존 논문들과 다른 방식으로 접근했던 점이 인상깊었습니다. 대부분의 Image Denoising Task 들은 supervised learning...","categories": ["Paper Review"],
+        "tags": ["Vision"],
+        "url": "http://localhost:4000/paper%20review/PR-002/",
+        "teaser":null},{
+        "title": "[논문읽기] Structured Knowledge Distillation for Semantic Segmentation",
+        "excerpt":"이 글은 2019 CVPR 논문, Structured Knowledge Distillation for Semantic Segmentation 를 참고하여 작성하였습니다. Knowledge distillation은 실제로 서비스에 적용하는 측면에서 굉장히 중요한 부분인 것 같아서 관심이 많은 연구주제 중 하나입니다. knowledge distillation에 대해서는 Ujjwal Upadhyay’s Blog에 정의와 필요성에 대해서 잘 설명되어 있으니 참고하면 좋을 것 같습니다. (+ Distilling the Knowledge...","categories": ["Paper Review"],
+        "tags": ["Knowledge Distillation","Semantic Segmentation"],
+        "url": "http://localhost:4000/paper%20review/PR-003/",
+        "teaser":null}]
